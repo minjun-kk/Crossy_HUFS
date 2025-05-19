aad7e946e74a2c4496a2738c010f19bccdc9e2ff
minjun <minjun0306@hufs.ac.kr>
2025-05-19 11:12:14 +0900

    맵 생성 조건 추가

--- Map/Mapmanager.cs
+++ Map/Mapmanager.cs
@@ -6,21 +6,57 @@ public class MapManager : MonoBehaviour
     public GameObject[] rowPrefabs; // 0: 잔디, 1: 도로, 2: 강
     public float rowLength = 10f;
     public Transform player;
+    public int bufferTiles = 10; // 플레이어 앞에 미리 생성할 타일 개수
 
     private Queue<GameObject> spawnedRows = new Queue<GameObject>();
-    private int lastRowZ = 0;
+    private int nextRowZ = 0; // 다음에 생성할 타일의 Z 위치
 
     void Start()
     {
-        // 30칸까지 한 번에 미리 생성
+        // 초기 40칸 생성
         for (int i = 0; i < 40; i++)
         {
-            if (i < 8)
-                SpawnRow(i, 0); // 0번(잔디)로 고정
+            if (i < 5)
+                SpawnRow(i, 0); // 처음 5칸은 잔디
             else
-                SpawnRandomRow(i);
+                SpawnRandomRowLimited(i, 0, 1); // 5~39칸은 잔디/도로 랜덤
         }
-        lastRowZ = 30;
+        nextRowZ = 40; // 다음 생성할 Z 위치
+    }
+
+    void Update()
+    {
+        int playerZ = Mathf.FloorToInt(player.position.z / rowLength);
+
+        // 플레이어가 10칸 전쯤 오면 맵 생성
+        while (playerZ + bufferTiles > nextRowZ)
+        {
+            if (nextRowZ < 40)
+                SpawnRandomRowLimited(nextRowZ, 0, 1); // 잔디/도로 랜덤
+            else if (nextRowZ < 60)
+                SpawnRow(nextRowZ, 2); // 40~59칸은 강 고정
+            else
+                SpawnRandomRowLimited(nextRowZ, 0, 1); // 60칸 이후 잔디/도로 랜덤
+
+            nextRowZ++;
+        }
+
+        // 지나간 타일 삭제 (플레이어 뒤 5칸 지나면 제거)
+        while (spawnedRows.Count > 0)
+        {
+            GameObject oldestRow = spawnedRows.Peek();
+            float oldestZ = oldestRow.transform.position.z;
+
+            if (oldestZ < player.position.z - 5 * rowLength)
+            {
+                Destroy(oldestRow);
+                spawnedRows.Dequeue();
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 
     void SpawnRow(int z, int type)
@@ -29,28 +65,20 @@ public class MapManager : MonoBehaviour
             rowPrefabs[type],
             new Vector3(0, 0, z * rowLength),
             Quaternion.identity,
-            this.transform
+            transform
         );
         spawnedRows.Enqueue(row);
     }
 
-    void SpawnRandomRow(int z)
+    void SpawnRandomRowLimited(int z, int type1, int type2)
     {
         float rand = Random.value;
         int type;
-        if (rand < 0.6f)
-            type = 0; // 잔디
-        else if (rand < 0.85f)
-            type = 1; // 도로
+        if (rand < 0.5f)
+            type = type1;
         else
-            type = 2; // 강
+            type = type2;
 
-        GameObject row = Instantiate(
-            rowPrefabs[type],
-            new Vector3(0, 0, z * rowLength),
-            Quaternion.identity,
-            this.transform
-        );
-        spawnedRows.Enqueue(row);
+        SpawnRow(z, type);
     }
 }
